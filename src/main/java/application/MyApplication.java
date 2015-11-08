037301e4f79bb1a4c01505c287483e50757234a3
wendywang <wangwenusa11@gmail.com>
2015-11-07 16:53:37 -0800

    Added .gitignore and deleted web.xml and added Application root class

---
+++ src/main/java/application/MyApplication.java
@@ -0,0 +1,21 @@
+package application;
+
+import net.gazsi.laszlo.sandbox.ws.RestExample;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by wendywang on 2015-11-07.
+ */
+public class MyApplication extends Application{
+
+    @Override
+    public Set<Class<?>> getClasses(){
+        Set<Class<?>> s = new HashSet<Class<?>>();
+        s.add(RestExample.class);
+        return s;
+    }
+
+}
