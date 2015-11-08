6018d9e67568f6d335f07e894e5a7b6d7c087c93
wendywang <wangwenusa11@gmail.com>
2015-11-08 15:24:23 -0800

    Modified pom.xml and added ConnectionFactory class and modified RestExample class

---
+++ src/main/java/database/utility/ConnectionFactory.java
@@ -0,0 +1,24 @@
+package database.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Created by wendywang on 2015-11-08.
+ */
+public class ConnectionFactory {
+
+    public static Connection getConnection() throws Exception{
+        try{
+            String connectionURL = "jdbc:mysql://localhost:3306/vegan_kitchen_api";
+            Connection connection = null;
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection(connectionURL, "root", "abcde");
+            return connection;
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+
+}
--- src/main/java/net/gazsi/laszlo/sandbox/ws/RestExample.java
+++ src/main/java/net/gazsi/laszlo/sandbox/ws/RestExample.java
@@ -1,5 +1,6 @@
 package net.gazsi.laszlo.sandbox.ws;
 
+import database.utility.ConnectionFactory;
 import model.Recipe;
 
 import javax.ws.rs.GET;
@@ -14,9 +15,10 @@ public class RestExample {
     @GET
     @Path("{id}")
     @Produces("application/json")
-    public Recipe sayHello(@PathParam("id") int id){
+    public Recipe sayHello(@PathParam("id") int id) throws Exception{
        // RecipeManager manager = new MockRecipeManager();
         //return manager.getRecipeByID(id);
+        ConnectionFactory.getConnection();
         return new Recipe("Dishname", 123, "imageURL", 1, "String summary");
 
     }
