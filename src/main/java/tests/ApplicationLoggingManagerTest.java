6e65dffed1d04dd9cfba9192b0e7fe2f0ecadcf3
wendywang <wangwenusa11@gmail.com>
2015-11-21 13:21:27 -0800

    Added a few test classes

---
+++ src/main/java/tests/ApplicationLoggingManagerTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wendywang on 2015-11-21.
+ */
+public class ApplicationLoggingManagerTest {
+
+    @Test
+    public void testGetApplicationLogging() throws Exception {
+
+    }
+}
